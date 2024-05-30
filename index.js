4e04d1726b9124bd49a8e72979217ea1bfb91d00
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2024-05-31 06:52:12 +0800

    chore: remove tag replies

--- index.js
+++ index.js
@@ -43,14 +43,6 @@ client.once('ready', () => {
   console.log(`成功登入 ${client.user.tag}!`)
 })
 
-const replies = ['五週年活動將在 {} 開始', '我知道你很期待，但五週年活動還要 {}', '再等 {} 五週年活動就開始了', '你還有 {} 可以準備你的超級英雄裝備', '不知道今年的攻略組有誰', 'En una antigua España, hace mucho, mucho tiempo......', '{} 就可以開始探索歌聖的故事了', '如果對手是你們的話，會贏喔', '聽說這次活動會有 Nitro 和 Nitro Basic 作為獎勵，真是大手筆', '聽說這次的獎勵全看你們 {} 的表現', '那個抽獎規則有夠複雜，不知道你們搞懂了嗎', '請期待 {} 的活動', '你是準備好了所以才有空在這邊亂 ping 我嗎？', '聽說本次活動主題是西班牙文，但為什麼我卻講中文？', 'Preparativos en curso, por favor espere pacientemente', 'Faltan {} para el inicio del evento', 'Por favor, espere {} más, el evento está a punto de comenzar', 'Si eres enemigo, te venceré']
-
-client.on('messageCreate', message => {
-  if (message.mentions.has(process.env.ID)) {
-    message.reply(replies[~~(Math.random() * replies.length)].replaceAll('{}', '<t:1717171200:R>'))
-  }
-})
-
 function ButtonInterationHandler(interaction) {
   if (interaction.customId.startsWith('level_')) levelHandler.execute(interaction, interaction.client)
 }
